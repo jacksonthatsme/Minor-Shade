ec1c0930c2be097c7c51ebea9111e721b5a34fa5
Jackson Lynch <Jacksonthatsme@gmail.com>
2014-05-02 02:32:40 -0400

    responsive slideshow works

--- js/mobile_slideshow.js
+++ js/mobile_slideshow.js
@@ -1,54 +1,36 @@
 var model = $("#model");
-  var classArr = ['front','up','up-left','left','down-left','down','down-right','right','up-right'];
-  var className = model.attr("class");
-  
-  function findIndex(){
-    var className = model.attr("class");    
-   
-    return $.inArray(className,classArr);
-    
+model.data('slide-index',0);  // store initial slide position
+var classArr = ['front','up','up-left','left','down-left','down','down-right','right','up-right'];
+var className = model.attr("class");
 
-  }
+function nextClass(){
+
+  var index = model.data('slide-index');
+  var currentClass = classArr[index];
+  model.removeClass(currentClass);
+  index += 1;
+  if(index == classArr.length){ index = 0; } // loop back to first
+  model.addClass(classArr[index]);
+  model.data('slide-index',index); // store the new value
+    
+}
+
+function prevClass(){
+
+  var index = model.data('slide-index');
+  var currentClass = classArr[index];
+  model.removeClass(currentClass);
+  index -= 1;
+  if(index < 0){ index = classArr.length-1; } // loop back to last
+  model.addClass(classArr[index]);
+  model.data('slide-index', index); // store the new value
   
-  function nextClass(){
-    var index = findIndex();
-    
-    if(index >= classArr.length ){
-      index = 0;
-    }else{
-      index = index + 1;
-    }
-        
-    var newClass = classArr[index];
-    
-    return newClass;
-  }
-  
-  function prevClass(){
-    var index = findIndex();
-    
-    if(index === 0){
-      index = 8;
-    }else{
-      index = index -1 ;
-    }
-    
-    console.log(index);
-    
-    var newClass = classArr[index];
-    
-    return newClass;
-  }
-  
-  function swapClass(newClass){
-     model.removeClass(className);
-     model.addClass(newClass);
-  }
-  
-  $('.slideshow-nav-next').click(function(){
-    swapClass(nextClass());
-    
-  });
-  $('.slideshow-nav-prev').click(function(){
-    swapClass(prevClass());
-  });
+}
+
+$('.slideshow-nav-next').click(function(){
+    nextClass();
+});
+$('.slideshow-nav-prev').click(function(){
+    prevClass();
+
+});
